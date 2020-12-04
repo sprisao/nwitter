67acd419b48840fadd43ff297309612cf5d4c408
Bruce Choe <whdtmek@gmail.com>
2020-12-04 15:13:30 +0900

    2.0 Using Firebase Auth

---
+++ src/fbase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+
+// Your web app's Firebase configuration
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBY71oc96AOgUbYUQIzmgagj0dpWXLgNG4",
+    authDomain: "nwitter-18f1c.firebaseapp.com",
+    projectId: "nwitter-18f1c",
+    storageBucket: "nwitter-18f1c.appspot.com",
+    messagingSenderId: "504209506158",
+    appId: "1:504209506158:web:e27034d8734f40c1778b7f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
