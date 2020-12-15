3d2b14e9ca9323c515b8e408e1ff4f5dc4de83b0
Bruce Choe <whdtmek@gmail.com>
2020-12-15 19:13:03 +0900

    5.2 Update Profile Bugfix

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,42 @@
-import { authService } from 'fbase';
+import { authService} from 'fbase';
 import { useHistory } from "react-router-dom";
-import React from "react";
+import React, {useState} from "react";
 
-export default () => {
+export default ({ refreshUser, userObj }) => {
         const history = useHistory();
+        const [newDisplayName, setNewDisplayName] = useState(userObj.newDisplayName);
         const onLogOutClick = () => {
-            authService.signOut();
+            authService.signOut();  
             history.push("/")
         };
+        const onChange = (event) => {
+            const {
+                target: {value},
+            } = event;
+            setNewDisplayName(value);
+        }
+        const onSubmit = async (event) => {
+            event.preventDefault();
+            if (userObj.displayName !== newDisplayName) {
+                await userObj.updateProfile({
+                    displayName: newDisplayName,
+                });
+                refreshUser();
+            }
+        };
+
     return (
         <>
-        <button onClick={onLogOutClick}>Log Out</button>
+            <form onSubmit={onSubmit}>
+                <input
+                onChange={onChange}
+                type="text"
+                placeholder="Display name"
+                value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
+            <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
 };
